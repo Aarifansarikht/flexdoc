0cda1cca4dc2dc943bea1073920f6799e2972df7
Aaifansarikht <arifansarikhtz@gmail.com>
2024-02-06 20:54:42 +0530

    update

--- src/app/components/Porsche.tsx
+++ src/app/components/Porsche.tsx
@@ -6,6 +6,7 @@ import { GLTFLoader } from "three/examples/jsm/Addons.js";
 export function Porsche({ props }: any) {
   const [loading, setLoading] = useState(true);
   const [model, setModel] = useState<any>(null);
+  const [selectedColorIndex, setSelectedColorIndex] = useState(0);
 
   useEffect(() => {
     const loader = new GLTFLoader();
@@ -22,9 +23,66 @@ export function Porsche({ props }: any) {
     );
   }, []);
 
+  useEffect(() => {
+    if (model) {
+      const plane = model.getObjectByName("Plane002_0");
+      if (plane) {
+        // Apply the selected color to the object
+        plane.material.color.set(palette[selectedColorIndex]);
+      }
+    }
+  }, [selectedColorIndex, model]);
+
+  const palette = [
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+    "#69D2E7",
+    "#A7DBD8",
+    "#E0E4CC",
+    "#F38630",
+    "#FA6900",
+    "#655643",
+    "#80BCA3",
+    "#F6F7BD",
+    "#E6AC27",
+    "#BF4D28",
+  ];
+
+  const changeColor = () => {
+    // Cycle through colors
+    setSelectedColorIndex((prevIndex) => (prevIndex + 1) % palette.length);
+  };
+
   return (
     <>
-      {loading ? <LoadingIndicator /> : <primitive object={model} {...props} />}
+      {loading ? (
+        <LoadingIndicator />
+      ) : (
+        <>
+          <primitive object={model} {...props} />
+          <Html>
+            <div
+              style={{
+                position: "absolute",
+                top: "10px",
+                left: "10px",
+                zIndex: 10,
+                padding: "20px 40px",
+              }}
+            >
+              <button
+                onClick={changeColor}
+                style={{
+                  padding: "10px 40px",
+                }}
+              >
+                Change Color
+              </button>
+            </div>
+          </Html>
+        </>
+      )}
     </>
   );
 }
