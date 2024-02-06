af0d71bab309f2410c8487a16d2413f7cf9b5cf1
Aaifansarikht <arifansarikhtz@gmail.com>
2024-02-06 16:06:48 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [changelight, setChangeLight] = useState(false);
+
   const togglelight = () => {
     setChangeLight(!changelight);
   };
